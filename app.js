27c85d867ac2e465f35b11f9bbac84d7b9dcb669
victor basurto <my.mvplace@gmail.com>
2016-04-28 12:06:03 -0700

    added description to forecast, max and min temperature added and code formatted

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /**
 * WeatherApp Module
 *
-* Description
+* Description: Small Weather APP, retrieve weather forecast from weatherAPI
 */
 var WeatherApp = angular.module('WeatherApp', [ 'ngRoute', 'ngResource' ]);
--- js/services/services.js
+++ js/services/services.js
@@ -4,5 +4,5 @@
 WeatherApp.service('SearchCity', function() {
 	var self = this;
 
-	this.city = 'San Diego, us';
+	this.city = 'San Diego, us';	// default search
 });
